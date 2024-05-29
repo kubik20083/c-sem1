e9e417588d3189655409313487188ad8134c0d13
kubik20083 <kubik20083@mail.ru>
2024-05-29 14:19:30 +0300

    task1

--- task1.c
+++ task1.c
@@ -1,4 +1,4 @@
-// На стандартном потоке ввода задается натуральное число N (N > 0), 
+// На стандартном потоке ввода задается натуральное число N (N > 0),
 // после которого следует последовательность из N целых чисел.
 // На стандартный поток вывода напечатайте,
 // сколько раз в этой последовательности встречается максимум.
@@ -6,6 +6,25 @@
 
 int main(int argc, char const *argv[])
 {
-    
+    int N;
+    int temp, count, max = 0;
+    printf("Enter number: ");
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++)
+    {
+        scanf("%d", &temp);
+
+        if (temp > max)
+        {
+            max = temp;
+            count = 1;
+        }
+        else if (temp == max)
+        {
+            count++;
+        }
+    }
+    printf("Number of maxima = %d\n", count);
+
     return 0;
 }
